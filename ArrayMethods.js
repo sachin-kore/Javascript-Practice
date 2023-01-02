350e9e2f633bee897d4504d33feb33c5e3e8306b
Sachin Kore <sachinkore085@gmail.com>
2023-01-02 19:23:48 +0530

    22/2022 commit

---
+++ ArrayMethods.js
@@ -0,0 +1,12 @@
+const str1 = 'Hello this is 1'
+const str2 = 'Hello this is 2'
+
+console.log(str1.concat(' ', str2));
+
+console.log(str1.includes('are'));
+
+console.log(str1.indexOf('this'));
+
+console.log(str1.slice(0, 5));
+
+console.log(str1.split(' '));
---
+++ GetOtp.js
@@ -0,0 +1,4 @@
+const r = Math.random();
+console.log(r);
+const otp = Math.round(r * 9000 + 1000)
+console.log(otp);
