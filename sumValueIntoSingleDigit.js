050bed7c3392d9da624cd2e24531c882fe55158c
Sachin Kore <sachinkore085@gmail.com>
2023-01-19 09:58:20 +0530

    22/2022 commit

---
+++ sumValueIntoSingleDigit.js
@@ -0,0 +1,14 @@
+const num = 543175377;
+function addData(num) {
+    let convertedString = "" + num;
+    let convertedArray = convertedString.split("").map((data) => {
+        return parseInt(data);
+    });
+    const finalOutput = convertedArray.reduce((a, b) => a + b)
+    if (finalOutput > 10) {
+        addData(finalOutput);
+    } else {
+        console.log(finalOutput);
+    }
+}
+addData(num);
